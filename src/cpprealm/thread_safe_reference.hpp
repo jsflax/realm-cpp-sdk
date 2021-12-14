6a639aa10c31d43fbbf4615720982238981d6857
Jason Flax <jsflax@gmail.com>
2021-12-14 15:20:35 +0000

    Add license headers

--- src/cpprealm/thread_safe_reference.hpp
+++ src/cpprealm/thread_safe_reference.hpp
@@ -1,8 +1,27 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2021 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
 #ifndef realm_thread_safe_reference_hpp
 #define realm_thread_safe_reference_hpp
 
 #include <cpprealm/type_info.hpp>
 #include <realm/object-store/thread_safe_reference.hpp>
+#include <realm/object-store/shared_realm.hpp>
 
 namespace realm {
 
